341b79de9a5968c0bd1178744a097ef74df2959d
Jeneva Scherr <scherrjeneva@gmail.com>
2019-02-14 23:29:51 -0800

    footer.php in PSR-2 format

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * header_inc.php provides the initial HTML for our site files 
- * Footer includes copyright and W3 validator. 
+ * header_inc.php provides the initial HTML for our site files
+ * Footer includes copyright and W3 validator.
  * @author Esteban Silva comsat61@gmail.com
  * @author Jeneva Scherr j3j3sherr@yahoo.com
  * @author Thom Harrrington thomas.harrington@seattlecentral.edu
@@ -9,15 +9,15 @@
  * @version 1.0 2019-02-14
  * @license https://www.apache.org/licenses/LICENSE-2.0
  * @todo none
- */ 
+ */
 ?>
-<!-- START footer.php -->
-        <footer>
-            <BR />
-            <hr class="footer">
-            <p style="text-align:center;"><small>&copy; 2018 - <?=date("Y")?> by Group, All Rights Reserved ~ <a href="https://validator.w3.org/check/referer" target="_blank">Valid HTML5</a> ~ <a href="https://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
-        </footer>
-<!-- END WRAPPER -->
+	<!-- START footer.php -->
+	<footer>
+	<BR />
+	<hr class="footer">
+	<p style="text-align:center;"><small>&copy; 2018 - <?=date("Y")?> by Group, All Rights Reserved ~ <a href="https://validator.w3.org/check/referer" target="_blank">Valid HTML5</a> ~ <a href="https://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+		</footer>
+		<!-- END WRAPPER -->
 
-</body>
-</html>
+		</body>
+		</html>
